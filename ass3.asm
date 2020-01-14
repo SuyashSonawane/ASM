16db1b4b085d9e320f0f2ecf0a337576f58d0e24
Suyash Sonawane <suyashsonawane007@gmail.com>
2020-01-14 15:15:21 -0500

    ass3

--- ass3.asm
+++ ass3.asm
@@ -3,8 +3,17 @@ section .data
 	msg0 db 10,"ASSIGNMENT 3",10
 	msg0_len equ $- msg0
 
-	msg1 db 10,"1.HEX to BCD",10,"2.BCD to HEX",10,"3.EXIT",10
+	msg1 db 10,"1.HEX to BCD",10,"2.BCD to HEX",10,"3.EXIT",10,10
 	msg1_len equ $- msg1
+
+	invalid_msg db 10,"Invalid Option Entered",10
+	invalid_msg_len equ $- invalid_msg
+
+	msg2 db 10,"Enter 4 digit HEX number",10
+	msg2_len equ $- msg2
+
+	msg3 db 10,"Enter 4 digit BCD number",10
+	msg3_len equ $- msg3
 
 
 
@@ -21,25 +30,65 @@ section .data
         mov rdi ,0
         syscall
 %endmacro
+%macro read 2
+    	mov rax ,0
+        mov rdi ,0
+        mov rsi, %1
+        mov rdx, %2
+        syscall
+%endmacro
 
 
 section .bss
 	char_ans resb 16
+	buf resb 2
 
 
 section .text
 	global _start
 		_start:
-			print msg0,msg0_len
-			print msg1,msg1_len
+			print msg0,msg0_len   ; ASS3
+			menu:
+				call Display_menu	
+				read buf,2
+				mov al ,[buf]
+				c1:
+					cmp al ,'1'
+					jne c2
+					call HEX_BCD
+					jmp menu
+				c2:
+					cmp al,'2'
+					jne c3
+					call BCD_HEX
+					jmp menu
+				c3:
+					cmp al,'3'
+					jne invalid
+					exit
+				invalid:
+					print invalid_msg,invalid_msg_len
+					jmp menu
 
-		exit
+
+		
 
 
 
+HEX_BCD:
+	print msg2,msg2_len
+	ret
+BCD_HEX:
+	print msg3,msg3_len
+	ret
+Display_menu:
+	mov rax ,1
+    mov rdi, 1
+    mov rsi ,msg1
+    mov rdx ,msg1_len
+    syscall
 
-
-
+	ret
 
 
 Display:
