cfde4d675a1203c9fac8f020a8b0fdaec1612b10
Suyash Sonawane <suyashsonawane007@gmail.com>
2020-02-25 04:05:03 -0500

    FAR

--- ass5.asm
+++ ass5.asm
@@ -46,6 +46,34 @@ section .data
         mov rdx, %2
         syscall
 %endmacro
+%macro fopen 1
+	mov rax,2
+	mov rdi %1
+	mov rsi,2
+	mov rdx , 07770
+	syscall
+%endmacro
+%macro fread 3
+	mov rax,0
+	mov rdi %1
+	mov rsi,%2
+	mov rdx , %3
+	syscall
+%endmacro
+%macro fwrite 3
+	mov rax,1
+	mov rdi %1
+	mov rsi,%2
+	mov rdx , %3
+	syscall
+%endmacro
+%macro fclose 1
+	mov rax,3
+	mov rdi %1
+	syscall
+%endmacro
+
+
 
 
 section .bss
